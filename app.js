ec19d6248f330667beb11d06568cc891d6e8e93b
Jakub Fronczyk <jakubfronczyk@gmail.com>
2022-09-08 16:23:23 +0200

    added result function

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const btnRules = document.querySelector(".rules-btn")
 const btnClose = document.querySelector(".close-btn")
 const modalRules = document.querySelector(".modal")
 
+const resultWinner = document.querySelector(".results__winner")
+const resultText = document.querySelector(".results__text")
+
 const CHOICES =[
     {
         name: "paper",
@@ -22,7 +25,7 @@ const CHOICES =[
     }
 ]
 const choiceButtons = document.querySelectorAll('.choice-btn')
-const gameDivs = document.querySelector('.game')
+const gameDiv = document.querySelector('.game')
 const resultsDiv = document.querySelector('.results')
 const resultDivs = document.querySelectorAll('.results__result')
 
@@ -38,6 +41,7 @@ choiceButtons.forEach(button => {
 function choose(choice) {
     const aichoice= aiChoose(choice)
     displayResults([choice, aichoice])
+    displayWinner([choice, aichoice])
 }
 
 function aiChoose() {
@@ -50,11 +54,35 @@ function displayResults(results) {
         setTimeout(()=>{
             resultDiv.innerHTML=`
             <div class="choice ${results[idx].name}">
-            <img src="images/icon-${results[idx].name}.svg" alt="${results[idx].name}"/>
+                <img src="images/icon-${results[idx].name}.svg" alt="${results[idx].name}"/>
             </div>
             `
-        }, idx * 1000)
+        }, idx * 1000)//first index[0] its going to be 0
     })
+
+    gameDiv.classList.toggle("hidden")
+    resultsDiv.classList.toggle("hidden")
+}
+
+function displayWinner(results){
+    setTimeout(()=>{
+        const userWins = isWinner(results)
+        const aiWins = isWinner(results.reverse())
+
+        if (userWins){
+            resultText.innerText = "you win"
+        } else if (aiWins){
+            resultText.innerText = "you lose"
+        }else{
+            resultText.innerText = "draw"
+        }
+        resultWinner.classList.toggle("hidden")
+        resultsDiv.classList.toggle("show-winner")
+}, 1000)
+}
+
+function isWinner(results){
+    return results[0].beats === results[1].name
 }
 // show/hide rules
 btnRules.addEventListener("click", () =>{
